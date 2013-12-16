8ed8a29dcad4825fa77cc42b17cfb999ec9ea9fd
Andreas Lüthi <andreas.luethi@gmx.net>
2013-12-16 17:53:03 +0100

    Experimental change for OpenVMS, replace variable "${variable}" with variable value.

--- src/main/java/org/jvnet/hudson/plugins/VariableReplacerUtil.java
+++ src/main/java/org/jvnet/hudson/plugins/VariableReplacerUtil.java
@@ -3,19 +3,20 @@ package org.jvnet.hudson.plugins;
 import java.util.Map;
 
 public class VariableReplacerUtil {
-	public static String replace(String originalCommand, Map<String, String> vars) {
-		if(originalCommand == null){
-			return null;
-		}
-		vars.remove("_"); //why _ as key for build tool?
-		StringBuilder sb = new StringBuilder();
-		for (String variable : vars.keySet()) {
-			if (originalCommand.contains(variable) ) {
-				sb.append(variable).append("=\"").append(vars.get(variable)).append("\"\n");
-			}
-		}
-		/////sb.append("\n");
-		sb.append(originalCommand);
-		return sb.toString();
-	}
+    public static String replace(String originalCommand, Map<String, String> vars) {
+        if (originalCommand == null) {
+            return null;
+        }
+        vars.remove("_"); //why _ as key for build tool?
+        StringBuilder sb = new StringBuilder();
+        for (String variable : vars.keySet()) {
+            originalCommand.replace("${" + variable + "}", vars.get(variable));
+//            if (originalCommand.contains(variable)) {
+//                sb.append(variable).append("=\"").append(vars.get(variable)).append("\"\n");
+//            }
+        }
+        /////sb.append("\n");
+        sb.append(originalCommand);
+        return sb.toString();
+    }
 }
